5db54fc971d6afffa82ead92085874c4955ae5d4
jx06T <119775148+jx06T@users.noreply.github.com>
2025-04-23 12:49:22 +0800

    Update page.tsx

--- src/app/hidden/page.tsx
+++ src/app/hidden/page.tsx
@@ -21,8 +21,7 @@ const HiddenPage = async () => {
     const text = `神回到了塔可拉島。祂感念你們救贖了這座島嶼，贈與你們一人一個十米高的黃金雕像。鬍嗑船長瞠目結舌，但因為感謝你們解放了這座島讓他能夠離開，他毅然決然決定載你們回去。到了旅人故鄉的島嶼(絕對沒有海關這種東西)你們用一點黃金換了一間大房子，房子的周圍擺著你們的雕像。
 於是你們過著幸福快樂的生活，躺著就可以過完餘生。
 多年以後，突然空氣中飄出一個聲音，是一個甜美的女聲，隨後是一個似曾相似的船夫的聲音。
-「塔可拉島已然至善至美，多虧了你們，因此神決定讓你們無限次來這裡度假，當然，這座島很隱密，所以只有你們能來，至於通關密語––」
-`;
+「塔可拉島已然至善至美，多虧了你們，因此神決定讓你們無限次來這裡度假，當然，這座島很隱密，所以只有你們能來，至於通關密語––」`;
 
     if (!sessionData) {
         return (
@@ -61,7 +60,7 @@ const HiddenPage = async () => {
                     <h3 className="text-md text-gray-500 mt-2">破解時間：{crackTime}</h3>
                     <pre className="mt-8 p-4 rounded-xl border border-gray-600 whitespace-pre-wrap">{text}
 <br></br>
- <h2 className="text-lg text-gray-400 mt-0">春眠不覺曉，電資樂逍遙</h2>
+ <h2 className="text-lg text-gray-400 mt-1">春眠不覺曉，電資樂逍遙</h2>
 						</pre>
                     <div className=' w-full  text-right mt-3 pr-1'>
                         <Link className="text-gray-500 text-base underline" href="/?auto=1">首頁</Link>
